85d950a5c34d037b4f85a1fe86396a6d7232af97
anthonyhami <66198890+anthonyhami@users.noreply.github.com>
2021-01-22 21:19:05 +0100

    Add files via upload

---
+++ ACGAN.py
@@ -0,0 +1,130 @@
+# example of defining the discriminator model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Conv2D
+from keras.layers import LeakyReLU
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import BatchNormalization
+from keras.initializers import RandomNormal
+from keras.optimizers import Adam
+from keras.utils.vis_utils import plot_model
+
+# define the standalone discriminator model
+def define_discriminator(in_shape=(28,28,1), n_classes=10):
+	# weight initialization
+	init = RandomNormal(stddev=0.02)
+	# image input
+	in_image = Input(shape=in_shape)
+	# downsample to 14x14
+	fe = Conv2D(32, (3,3), strides=(2,2), padding='same', kernel_initializer=init)(in_image)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	fe = Dropout(0.5)(fe)
+	# normal
+	fe = Conv2D(64, (3,3), padding='same', kernel_initializer=init)(fe)
+	fe = BatchNormalization()(fe)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	fe = Dropout(0.5)(fe)
+	# downsample to 7x7
+	fe = Conv2D(128, (3,3), strides=(2,2), padding='same', kernel_initializer=init)(fe)
+	fe = BatchNormalization()(fe)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	fe = Dropout(0.5)(fe)
+	# normal
+	fe = Conv2D(256, (3,3), padding='same', kernel_initializer=init)(fe)
+	fe = BatchNormalization()(fe)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	fe = Dropout(0.5)(fe)
+	# flatten feature maps
+	fe = Flatten()(fe)
+	# real/fake output
+	out1 = Dense(1, activation='sigmoid')(fe)
+	# class label output
+	out2 = Dense(n_classes, activation='softmax')(fe)
+	# define model
+	model = Model(in_image, [out1, out2])
+	# compile model
+	opt = Adam(lr=0.0002, beta_1=0.5)
+	model.compile(loss=['binary_crossentropy', 'sparse_categorical_crossentropy'], optimizer=opt)
+	return model
+
+# define the discriminator model
+model = define_discriminator()
+# summarize the model
+model.summary()
+# plot the model
+plot_model(model, to_file='discriminator_plot.png', show_shapes=True, show_layer_names=True)
+
+
+
+# example of defining the generator model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers import Conv2DTranspose
+from keras.layers import Embedding
+from keras.layers import Concatenate
+from keras.layers import Activation
+from keras.layers import BatchNormalization
+from keras.initializers import RandomNormal
+from keras.utils.vis_utils import plot_model
+
+# define the standalone generator model
+def define_generator(latent_dim, n_classes=10):
+	# weight initialization
+	init = RandomNormal(stddev=0.02)
+	# label input
+	in_label = Input(shape=(1,))
+	# embedding for categorical input
+	li = Embedding(n_classes, 50)(in_label)
+	# linear multiplication
+	n_nodes = 7 * 7
+	li = Dense(n_nodes, kernel_initializer=init)(li)
+	# reshape to additional channel
+	li = Reshape((7, 7, 1))(li)
+	# image generator input
+	in_lat = Input(shape=(latent_dim,))
+	# foundation for 7x7 image
+	n_nodes = 384 * 7 * 7
+	gen = Dense(n_nodes, kernel_initializer=init)(in_lat)
+	gen = Activation('relu')(gen)
+	gen = Reshape((7, 7, 384))(gen)
+	# merge image gen and label input
+	merge = Concatenate()([gen, li])
+	# upsample to 14x14
+	gen = Conv2DTranspose(192, (5,5), strides=(2,2), padding='same', kernel_initializer=init)(merge)
+	gen = BatchNormalization()(gen)
+	gen = Activation('relu')(gen)
+	# upsample to 28x28
+	gen = Conv2DTranspose(1, (5,5), strides=(2,2), padding='same', kernel_initializer=init)(gen)
+	out_layer = Activation('tanh')(gen)
+	# define model
+	model = Model([in_lat, in_label], out_layer)
+	return model
+
+# define the size of the latent space
+latent_dim = 100
+# define the generator model
+model = define_generator(latent_dim)
+# summarize the model
+model.summary()
+# plot the model
+plot_model(model, to_file='generator_plot.png', show_shapes=True, show_layer_names=True)
+
+
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model):
+	# make weights in the discriminator not trainable
+	d_model.trainable = False
+	# connect the outputs of the generator to the inputs of the discriminator
+	gan_output = d_model(g_model.output)
+	# define gan model as taking noise and label and outputting real/fake and label outputs
+	model = Model(g_model.input, gan_output)
+	# compile model
+	opt = Adam(lr=0.0002, beta_1=0.5)
+	model.compile(loss=['binary_crossentropy', 'sparse_categorical_crossentropy'], optimizer=opt)
+	return model
+  
+  
